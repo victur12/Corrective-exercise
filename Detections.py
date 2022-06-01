04faa71d1be3cbd890028fee32c16c39b6c7f4dd
Victor Gonzalez <vgonzalezvictor0@gmail.com>
2022-06-01 16:09:17 -0500

    Create and trainning model and detection

---
+++ Detections.py
@@ -0,0 +1,88 @@
+import pickle 
+import mediapipe as mp 
+import cv2 
+import numpy as np
+import pandas as pd
+
+with open('exercises.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+mp_drawing = mp.solutions.drawing_utils 
+mp_holistic = mp.solutions.holistic 
+
+cap = cv2.VideoCapture("squat3.mp4")
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False        
+        
+        results = holistic.process(image)
+     
+        image.flags.writeable = True   
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+    
+
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                 )
+
+        try:
+
+            pose = results.pose_landmarks.landmark
+            pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+            
+            row = pose_row
+            
+
+            X = pd.DataFrame([row])
+            body_language_class = model.predict(X)[0]
+            body_language_prob = model.predict_proba(X)[0]
+            print(body_language_class, body_language_prob)
+            
+            # Grab ear coords
+            coords = tuple(np.multiply(
+                            np.array(
+                                (results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x, 
+                                 results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y))
+                        , [640,480]).astype(int))
+            
+            cv2.rectangle(image, 
+                          (coords[0], coords[1]+5), 
+                          (coords[0]+len(body_language_class)*20, coords[1]-30), 
+                          (245, 117, 16), -1)
+            cv2.putText(image, body_language_class, coords, 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+     
+            cv2.rectangle(image, (0,0), (250, 60), (245, 117, 16), -1)
+            
+       
+            cv2.putText(image, 'CLASS'
+                        , (95,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, body_language_class.split(' ')[0]
+                        , (90,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+            # Display Probability
+            cv2.putText(image, 'PROB'
+                        , (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(round(body_language_prob[np.argmax(body_language_prob)],2))
+                        , (10,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
+        except:
+            pass
+                        
+        cv2.imshow('Exercise', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+
+tuple(np.multiply(np.array((results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].x, 
+results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_EAR].y)), [640,480]).astype(int))
--- pose_detection.py
+++ pose_detection.py
@@ -1,55 +1,63 @@
-from operator import delitem
-from turtle import color
-import cv2
-import mediapipe as mp
+import mediapipe as mp # Import mediapipe
+import cv2 # Import opencv
 import csv
-import numpy as np
 import os
+import numpy as np
 
-mp_drawing = mp.solutions.drawing_utils
-mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils # Drawing helpers
+mp_holistic = mp.solutions.holistic # Mediapipe Solutions
 
-cap = cv2.VideoCapture("push_up.mp4");
-class_name = "push_up"
-
-with mp_pose.Pose(
-    static_image_mode = False) as pose:
-
-    while True:
+class_name= "squat"
+cap = cv2.VideoCapture("squat2.mp4")
+# Initiate holistic model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    while cap.isOpened():
         ret, frame = cap.read()
-        if ret == False:
-            break
-        height, width, _ = frame.shape
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = pose.process(frame_rgb)
-
-        if results.pose_landmarks is not None:
-            mp_drawing.draw_landmarks (frame, results.pose_landmarks,
-                mp_pose.POSE_CONNECTIONS,
-                mp_drawing.DrawingSpec(color=(255, 255, 255), circle_radius =3),
-               )
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-
-try:
+        
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False        
+        
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
+        
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+        
+        # Recolor image back to BGR for rendering
+        image.flags.writeable = True   
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                 )
+        # Export coordinates
+        try:
             # Extract Pose landmarks
-    pose = results.pose_landmarks.landmark
-    pose_row = list(np.array([[landmark.x, landmark.y, landmark.z] for landmark in pose]).flatten())
-    row = pose_row
-    
-    # Append class name 
-    row.insert(0, class_name)
-    
-    # Export to CSV
-    with open('coords.csv', mode='a', newline='') as f:
-        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(row) 
-except:
-    pass
+            pose = results.pose_landmarks.landmark
+            pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in pose]).flatten())
+            
+        
+            # Concate rows
+            row = pose_row
+            
+            # Append class name 
+            row.insert(0, class_name)
+            
+            # Export to CSV
+            with open('coords.csv', mode='a', newline='') as f:
+                csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow(row) 
+            
+        except:
+            pass
+                        
+        cv2.imshow('Raw Webcam Feed', image)
 
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-print('termino')
 cap.release()
 cv2.destroyAllWindows()
-            
